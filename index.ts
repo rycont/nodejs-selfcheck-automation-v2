3e1265682d097a5e77f3ffbaf278820e819463f6
rycont <reactdev@kakao.com>
2020-10-08 00:27:38 +0900

    feat  : non-경기 region school support tested with 화천고등학교(강원)

--- index.ts
+++ index.ts
@@ -1,96 +1,122 @@
-import { JSEncrypt } from 'nodejs-jsencrypt'
-import fetch from 'node-fetch'
-import { Level, Region, SchoolInfo } from './types'
+import { JSEncrypt } from "nodejs-jsencrypt";
+import fetch from "node-fetch";
+import { Level, Region, SchoolInfo } from "./types";
 
-const encryptor = new JSEncrypt()
+const encryptor = new JSEncrypt();
 
 export const setPrivateKey = (privateKey: string) => {
-    encryptor.setPrivateKey(privateKey)
-}
+  encryptor.setPrivateKey(privateKey);
+};
 
-export const getSchoolID = async ({ region, level, query }: { region: keyof typeof Region, level: keyof typeof Level, query: string }): Promise<SchoolInfo[]> => {
-    if (!Level[level]) throw new Error('지역정보가 올바르지 않습니다')
-    if (!Region[region]) throw new Error('교급정보가 올바르지 않습니다')
-    return fetch(`https://hcs.eduro.go.kr/v2/searchSchool?lctnScCode=${Region[region] + 1}&schulCrseScCode=${Level[level] + 1}&orgName=${encodeURI(query)}&loginType=school`)
-        .then(e => e.json())
-        .then(e => {
-            if (e.schulList.length === 0) throw new Error("해당하는 학교를 찾을 수 없습니다.")
-            return e.schulList
-        })
-        .catch(e => {
-            throw e || new Error('학교 정보를 받아올 수 없습니다')
-        })
-}
+export const getSchoolID = async ({
+  region,
+  level,
+  query,
+}: {
+  region: keyof typeof Region;
+  level: keyof typeof Level;
+  query: string;
+}): Promise<SchoolInfo[]> => {
+  if (!Level[level]) throw new Error("지역정보가 올바르지 않습니다");
+  if (!Region[region]) throw new Error("교급정보가 올바르지 않습니다");
+  return fetch(
+    `https://hcs.eduro.go.kr/v2/searchSchool?lctnScCode=${
+      Region[region] + 1
+    }&schulCrseScCode=${Level[level] + 1}&orgName=${encodeURI(
+      query
+    )}&loginType=school`
+  )
+    .then((e) => e.json())
+    .then((e) => {
+      if (e.schulList.length === 0)
+        throw new Error("해당하는 학교를 찾을 수 없습니다.");
+      return e.schulList;
+    })
+    .catch((e) => {
+      throw e || new Error("학교 정보를 받아올 수 없습니다");
+    });
+};
 
 export const getToken = async ({
-    name,
-    birthday,
-    schoolId
+  name,
+  birthday,
+  schoolId,
+  domain,
 }: {
-    name: string;
-    birthday: string;
-    schoolId: string;
+  name: string;
+  birthday: string;
+  schoolId: string;
+  domain: string;
 }): Promise<string> => {
-    const encryptedInfo = {
-        birthday: encryptor.encrypt(birthday),
-        name: encryptor.encrypt(name)
-    };
-    return (fetch('https://goehcs.eduro.go.kr/v2/findUser', {
-        method: 'POST',
-        body: JSON.stringify({
-            ...encryptedInfo,
-            orgCode: schoolId,
-            loginType: "school"
-        }),
-        headers: {
-            "Content-Type": "application/json"
-        }
-    })).then(e => e.json()).then(e => {
-        if (e.isError) throw new Error(e.message)
-        return e.token
-    }).catch((e) => {
-        throw e || new Error("유저 정보 찾기 실패")
+  const encryptedInfo = {
+    birthday: encryptor.encrypt(birthday),
+    name: encryptor.encrypt(name),
+  };
+  if (!domain) throw new Error("학교 정보가 올바르지 않습니다");
+  return fetch(`https://${domain}/v2/findUser`, {
+    method: "POST",
+    body: JSON.stringify({
+      ...encryptedInfo,
+      orgCode: schoolId,
+      loginType: "school",
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((e) => e.json())
+    .then((e) => {
+      if (e.isError) throw new Error(e.message);
+      return e.token;
     })
-}
+    .catch((e) => {
+      throw e || new Error("유저 정보 찾기 실패");
+    });
+};
 
 export const takeSurvey = async ({
-    token,
-    name
+  token,
+  name,
+  domain,
 }: {
-    token: string,
-    name: string
-}): Promise<{ registerDtm: string, inveYmd: string; state?: 'succeed' }> => {
-    return fetch('https://goehcs.eduro.go.kr/registerServey', {
-        method: 'POST',
-        headers: {
-            Authorization: token,
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            "rspns01": "1",
-            "rspns02": "1",
-            "rspns03": null,
-            "rspns04": null,
-            "rspns05": null,
-            "rspns06": null,
-            "rspns07": "0",
-            "rspns08": "0",
-            "rspns09": "0",
-            "rspns10": null,
-            "rspns11": null,
-            "rspns12": null,
-            "rspns13": null,
-            "rspns14": null,
-            "rspns15": null,
-            "rspns00": "Y",
-            "deviceUuid": "",
-            "upperUserNameEncpt": name
-        })
-    }).then(e => e.json()).then(e => {
-        if (!e.registerDtm) throw new Error("자가진단을 시행할 수 없습니다")
-        return {
-            ...e,
-            state: 'succeed'
-        }
-    })
-}
+  token: string;
+  name: string;
+  domain: string;
+}): Promise<{ registerDtm: string; inveYmd: string; state?: "succeed" }> => {
+  if (!domain) throw new Error("학교 정보가 올바르지 않습니다");
+  return fetch(`https://${domain}/registerServey`, {
+    method: "POST",
+    headers: {
+      Authorization: token,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      rspns01: "1",
+      rspns02: "1",
+      rspns03: null,
+      rspns04: null,
+      rspns05: null,
+      rspns06: null,
+      rspns07: "0",
+      rspns08: "0",
+      rspns09: "0",
+      rspns10: null,
+      rspns11: null,
+      rspns12: null,
+      rspns13: null,
+      rspns14: null,
+      rspns15: null,
+      rspns00: "Y",
+      deviceUuid: "",
+      upperUserNameEncpt: name,
+    }),
+  })
+    .then((e) => e.json())
+    .then((e) => {
+      if (!e.registerDtm) throw new Error("자가진단을 시행할 수 없습니다");
+      return {
+        ...e,
+        state: "succeed",
+      };
+    });
+};
--- start.ts
+++ start.ts
@@ -1,59 +1,68 @@
-import { config } from 'dotenv'
-import ora from 'ora'
+import { config } from "dotenv";
+import ora from "ora";
 import { getSchoolID, getToken, takeSurvey, setPrivateKey } from ".";
-import { Level } from './types';
+import { Level } from "./types";
 
-config()
-if (process.env.PRIVATE_KEY) setPrivateKey(process.env.PRIVATE_KEY)
-else throw new Error("Privatekey를 찾을 수 없습니다!")
+config();
+if (process.env.PRIVATE_KEY) setPrivateKey(process.env.PRIVATE_KEY);
+else throw new Error("Privatekey를 찾을 수 없습니다!");
 
 const getFromEnv = <T>(key: string): T => {
-    const v = process.env[key]
-    if (v) return v as unknown as T
-    throw new Error(`❌ "${key}"를 찾을 수 없습니다`)
-}
+  const v = process.env[key];
+  if (v) return (v as unknown) as T;
+  throw new Error(`❌ "${key}"를 찾을 수 없습니다`);
+};
 
-const getSchoolIDSpinner = ora('학교정보를 불러오는중...').start()
-const getTokenSpinner = ora('학생정보를 불러오는중...')
-const takeSurveySpinner = ora('자가진단을 수행하는중...')
+const getSchoolIDSpinner = ora("학교정보를 불러오는중...").start();
+const getTokenSpinner = ora("학생정보를 불러오는중...");
+const takeSurveySpinner = ora("자가진단을 수행하는중...");
+
+let uri: string;
 
 getSchoolID({
-    region: getFromEnv('REGION'),
-    level: getFromEnv('LEVEL'),
-    query: getFromEnv('SCHOOL_NAME')
+  region: getFromEnv("REGION"),
+  level: getFromEnv("LEVEL"),
+  query: getFromEnv("SCHOOL_NAME"),
 })
-    .catch(e => {
-        getSchoolIDSpinner.fail('학교정보를 받아오는데 실패했습니다')
-        throw e
-    })
-    .then(e => {
-        getSchoolIDSpinner.succeed(`학교명: ${e[0].kraOrgNm}`)
-        getTokenSpinner.start()
-        return getToken({
-            name: getFromEnv('STUDENT_NAME'),
-            birthday: getFromEnv('STUDENT_BIRTHDAY'),
-            schoolId: e[0].orgCode,
-        })
-    })
-    .catch(e => {
-        getTokenSpinner.fail('학생정보를 받아오는데 실패했습니다.')
-        throw e
-    })
-    .then(e => {
-        getTokenSpinner.succeed(`학생 이름: ${getFromEnv('STUDENT_NAME')}, 생년월일: ${getFromEnv('STUDENT_BIRTHDAY')}`)
-        takeSurveySpinner.start()
-        return takeSurvey({
-            token: e,
-            name: getFromEnv('STUDENT_NAME')
-        })
-    })
-    .catch(e => {
-        takeSurveySpinner.fail('자가진단 제출에 실패했습니다')
-        throw e
-    })
-    .then(e => {
-        return takeSurveySpinner.succeed(`🎉 오늘의 자가진단을 완료했습니다`)
-    })
-    .catch(e => {
-        console.log('\n😢 자가진단을 완료하지 못했습니다. 에러명: \n', e.message)
-    })
+  .catch((e) => {
+    getSchoolIDSpinner.fail("학교정보를 받아오는데 실패했습니다");
+    throw e;
+  })
+  .then((e) => {
+    uri = e[0].atptOfcdcConctUrl;
+    getSchoolIDSpinner.succeed(`학교명: ${e[0].kraOrgNm}`);
+    getTokenSpinner.start();
+    return getToken({
+      name: getFromEnv("STUDENT_NAME"),
+      birthday: getFromEnv("STUDENT_BIRTHDAY"),
+      schoolId: e[0].orgCode,
+      domain: uri,
+    });
+  })
+  .catch((e) => {
+    getTokenSpinner.fail("학생정보를 받아오는데 실패했습니다.");
+    throw e;
+  })
+  .then((e) => {
+    getTokenSpinner.succeed(
+      `학생 이름: ${getFromEnv("STUDENT_NAME")}, 생년월일: ${getFromEnv(
+        "STUDENT_BIRTHDAY"
+      )}`
+    );
+    takeSurveySpinner.start();
+    return takeSurvey({
+      token: e,
+      name: getFromEnv("STUDENT_NAME"),
+      domain: uri,
+    });
+  })
+  .catch((e) => {
+    takeSurveySpinner.fail("자가진단 제출에 실패했습니다");
+    throw e;
+  })
+  .then((e) => {
+    return takeSurveySpinner.succeed(`🎉 오늘의 자가진단을 완료했습니다`);
+  })
+  .catch((e) => {
+    console.log("\n😢 자가진단을 완료하지 못했습니다. 에러명: \n", e.message);
+  });
